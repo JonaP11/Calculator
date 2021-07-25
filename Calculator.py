533ce38d197e79605ef4175872e03e8bbc059c2a
JonaP11 <56172477+JonaP11@users.noreply.github.com>
2021-07-25 01:38:57 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,10 @@
+#Copyright (c) <year>, <copyright holder>
+#All rights reserved.
 
+#This source code is licensed under the BSD-style license found in the
+#LICENSE file in the root directory of this source tree. 
+
+def main():
+    print("First file for calculator and change to file")
+
+main()
